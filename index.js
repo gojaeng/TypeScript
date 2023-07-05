2c9eb3ba92dacfb90ca62cff287327fd9ab464e2
고재현 <gojh1013@gmail.com>
2023-07-05 15:15:51 +0900

    type alias

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var 이름 = 'kim';
-function 함수(x) {
-    return x * 2;
-}
-var john = { name: 'kim' };
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
+//narrowing
+function 내함수(x) {
+    var array = [];
+    if (typeof x === 'number') {
+        array[0] = x;
     }
-    return User;
-}());
+}
+내함수(123);
+//assertion
+function 내함수2(x) {
+    var array = [];
+    array[0] = x;
+}
+내함수2(3);
