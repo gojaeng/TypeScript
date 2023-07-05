45ed1106fecfb1b167bf1bebc3674d795d1c759c
고재현 <gojh1013@gmail.com>
2023-07-05 15:43:28 +0900

    literal types

---
+++ literal types.js
@@ -0,0 +1,15 @@
+var 접니다;
+function 함수(a) {
+    return 1;
+}
+함수("hello");
+function 가위바위보(a) {
+    return ['가위'];
+}
+가위바위보('가위');
+var 자료 = {
+    name: 'kim'
+};
+function 내함수1(a) {
+}
+내함수1(자료.name);
