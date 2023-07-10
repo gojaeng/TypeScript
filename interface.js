bf1a2ffc5556b27e632905d4ae9a97d41f1db3c6
고재현 <gojh1013@gmail.com>
2023-07-10 19:18:12 +0900

    interface & intersection

---
+++ interface.js
@@ -0,0 +1,3 @@
+var 네모 = { color: 'red', width: 100 };
+var 학생 = { name: 'kim' };
+var 선생 = { name: 'kim', age: 20 };
---
+++ interface.ts
@@ -0,0 +1,24 @@
+interface Square {
+    color:string;
+    width:number;
+}
+
+let 네모:Square = {color:'red', width:100}
+
+
+
+interface Student{
+    name : string;
+}
+interface Teacher extends Student{
+    age : number;
+}
+
+
+let 학생:Student = {name : 'kim'}
+let 선생:Teacher = {name: 'kim',age:20};
+
+
+type Animal2 = {name:string}
+type Cat = {age:number} & /*intersection type*/ Animal2
+
