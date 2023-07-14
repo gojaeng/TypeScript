421cb295e83ec55cf2d8bc0c76e8987347cbef18
고재현 <gojh1013@gmail.com>
2023-07-14 16:39:20 +0900

    Narrowing 방법 추가

---
+++ narrowing.js
@@ -0,0 +1,13 @@
+function 함수(animal) {
+    if ('swim' in animal) {
+        animal.swim;
+    }
+}
+var 날짜 = new Date();
+if (날짜 instanceof Date) {
+}
+function 함수(x) {
+    if (x.wheel === '4개') {
+        console.log('x는 car타입이에요');
+    }
+}
