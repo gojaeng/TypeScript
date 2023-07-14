62a2c75db20f9653e652bd9415b68f5c60b6032c
고재현 <gojh1013@gmail.com>
2023-07-14 16:47:44 +0900

    never 타입

---
+++ never 타입.js
@@ -0,0 +1,16 @@
+function 함수() {
+    //throw new Error()
+    while (true) {
+    }
+}
+function 함수(parameter) {
+    if (typeof parameter == 'string') {
+        console.log(parameter);
+    }
+    else {
+        console.log(parameter);
+    }
+}
+var 함수2 = function () {
+    throw new Error();
+};
