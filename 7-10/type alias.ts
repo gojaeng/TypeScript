3c2643ecdc4586a03de61c8cdc19761416eb3d4e
고재현 <gojh1013@gmail.com>
2023-07-29 14:22:14 +0900

    type alias

--- 7-10/type alias.ts
+++ 7-10/type alias.ts
@@ -1,29 +1,29 @@
-type Animal = {name:string, age:number}
-let 동물 : Animal = {name:'kim',age:20}
+// type Animal = {name:string, age:number}
+// let 동물 : Animal = {name:'kim',age:20}
 
-const 출생지역 = {region: 'seoul'};
-출생지역.region = 'busan'; 
+// const 출생지역 = {region: 'seoul'};
+// 출생지역.region = 'busan'; 
 
-type Girlfriend = {
-     name? : string
-}
-const 여친: Girlfriend ={
-    name: '엠버'
-}
-여친.name = '유라';
+// type Girlfriend = {
+//      name? : string
+// }
+// const 여친: Girlfriend ={
+//     name: '엠버'
+// }
+// 여친.name = '유라';
 
-type Name = string;
-type Age = number;
-type Person = Name | Age;
+// type Name = string;
+// type Age = number;
+// type Person = Name | Age;
 
-type PositionX = {x:number};
-type PositionY = {y:number};
-type NewType = PositionX & PositionY
-let position :NewType = {x:10, y:20};
+// type PositionX = {x:number};
+// type PositionY = {y:number};
+// type NewType = PositionX & PositionY
+// let position :NewType = {x:10, y:20};
 
 
-type Type = {
-    color? : string;
-    size : number;
-    readonly position : number[]
-}
+// type Type = {
+//     color? : string;
+//     size : number;
+//     readonly position : number[]
+// }
